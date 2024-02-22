6ecdb1c12b467aa762cf6b06fd3912620f70c8f5
Jake Willshire <jake.willshire@matthey.com>
2024-02-22 09:31:27 +0000

    Interface created for Object Management

---
+++ LMS_WPFApp/LMS_WPFApp/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_WPFApp
+{
+    internal class UserManager : ISystemObjectManager
+    {
+        void ISystemObjectManager.CreateNewField(string fieldName, object fieldValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISystemObjectManager.CreateNewObject(string objectName)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISystemObjectManager.DeleteField(string fieldName)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISystemObjectManager.DeleteObject(string objectName)
+        {
+            throw new NotImplementedException();
+        }
+
+        object ISystemObjectManager.GetFieldContents(string fieldName)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ISystemObjectManager.GetObjectName()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ISystemObjectManager.SetObjectName(string objectName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
