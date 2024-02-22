6e96ade7ba9bd46186a91f6801ae4711142d855e
th5528e <th5528e@gre.ac.uk>
2024-02-22 10:57:01 +0000

    added payment window UI start

--- LMS_WPFApp/LMS_WPFApp/paymentGateway.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/paymentGateway.xaml.cs
@@ -1,9 +1,28 @@
+<<<<<<< HEAD
 ﻿using System.Windows;
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+>>>>>>> d5f169f (added payment window UI start)
 
 namespace LMS_WPFApp
 {
     public partial class paymentGateway : Window
     {
+<<<<<<< HEAD
         private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
 
@@ -15,6 +34,18 @@ namespace LMS_WPFApp
         }
 
         private void cancelPayFeesButton_Click(object sender, RoutedEventArgs e)
+=======
+        public paymentGateway()
+        {
+            InitializeComponent();
+            DataTable.
+            ComboBox cb=new ComboBox();
+            cb.DataContext = db;
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+>>>>>>> d5f169f (added payment window UI start)
         {
 
         }
