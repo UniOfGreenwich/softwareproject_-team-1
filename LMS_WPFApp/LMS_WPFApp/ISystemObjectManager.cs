b28462552fd9276988ceafaf6957269942fa1ddb
Jake Willshire <jake.willshire@matthey.com>
2024-02-22 09:31:27 +0000

    Interface created for Object Management

---
+++ LMS_WPFApp/LMS_WPFApp/ISystemObjectManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_WPFApp
+{
+    internal interface ISystemObjectManager
+    {
+        void CreateNewObject(string objectName);
+        void DeleteObject(string objectName);
+        void CreateNewField(string fieldName, object fieldValue);
+        void DeleteField(string fieldName);
+        string GetObjectName();
+        void SetObjectName(string objectName);
+        object GetFieldContents(string fieldName);
+    }
+}
