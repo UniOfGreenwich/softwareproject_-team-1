4d2bc77c6dcf218c3f1cef966434273532c1fe9b
th5528e <th5528e@gre.ac.uk>
2024-02-29 10:24:57 +0000

    updated cs files for functionality

--- LMS_WPFApp/LMS_WPFApp/paymentGateway.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/paymentGateway.xaml.cs
@@ -1,33 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-//using System.Windows.Forms;
+﻿using System.Windows;
 
 namespace LMS_WPFApp
 {
     public partial class paymentGateway : Window
     {
-        public paymentGateway()
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            InitializeComponent();
-            //DataTable.
-            //ComboBox cb=new ComboBox();
-            //cb.DataContext = db;
 
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void payFeesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cancelPayFeesButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
