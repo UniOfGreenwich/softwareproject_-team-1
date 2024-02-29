8df68526079d23f3b3188bdb020a4962adcec962
Repi909 <j.willshire@ntlworld.com>
2024-02-29 11:52:13 +0000

    Added dummy csv file. Created methods within InventoryManager class to read from and write to csv file.

--- LMS_WPFApp/LMS_WPFApp/InventoryManager.cs
+++ LMS_WPFApp/LMS_WPFApp/InventoryManager.cs
@@ -1,44 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LMS_WPFApp
 {
-    internal class InventoryManager : ISystemObjectManager
+    public class InventoryManager : I_SystemObjectManager
     {
-        void ISystemObjectManager.CreateNewField(string fieldName, object fieldValue)
+        private List<List<string>>? inventoryList;
+        private static string databasePath = "C:/Data/InventoryDatabase.csv";  //change file path here if needed. Needs to be static in deployment.
+        private List<string> headers { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+        public string? ISBN {  get; set; }
+        public string? TotalQuantity { get; set; }
+        public string? RentedQuantity { get; set; }
+
+        void I_SystemObjectManager.OpenDatabaseFile()
+        {
+            using(var reader = new StreamReader(databasePath))
+            {
+                this.inventoryList = new List<List<string>>();
+                this.headers = new List<string>();
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    List<string> rowValues = new List<string>();
+
+                    foreach (var value in values)
+                    {
+                        rowValues.Add(value);
+                    }
+                    this.inventoryList.Add(rowValues);
+                }
+                reader.Close();
+            }
+            this.headers = inventoryList[0];
+        }
+        void I_SystemObjectManager.CloseDatabaseFile(List<List<string>> inventoryList)
+        {
+            string inventoryString = '';
+            string rowString = '';
+            using (var writer = new StreamWriter(databasePath))
+            {
+                foreach (var row in inventoryList)
+                {
+                    foreach (var value in row)
+                    {
+                        string valueString = value.ToString();
+                        rowString += (',' + valueString);
+                    }
+                    inventoryString += rowString + '\n';
+                }
+                writer.Write(inventoryString);
+                writer.Close();
+            }
+        }
+
+        void I_SystemObjectManager.CreateNewField(string fieldName, List<List<string>> inventoryList)
+        {
+            
+            headers.Add(fieldName);
+            foreach (var row in inventoryList)
+            {
+                row.Add(null);
+            }
+        }
+
+        void I_SystemObjectManager.CreateNewObject(string objectName)
+        {
+            List<string> headers = 
+            
+        }
+
+        void I_SystemObjectManager.DeleteField(string fieldName)
         {
             throw new NotImplementedException();
         }
 
-        void ISystemObjectManager.CreateNewObject(string objectName)
+        void I_SystemObjectManager.DeleteObject(string objectName)
         {
             throw new NotImplementedException();
         }
 
-        void ISystemObjectManager.DeleteField(string fieldName)
+        object I_SystemObjectManager.GetFieldContents(string fieldName)
         {
             throw new NotImplementedException();
         }
 
-        void ISystemObjectManager.DeleteObject(string objectName)
+        string I_SystemObjectManager.GetObjectName()
         {
             throw new NotImplementedException();
         }
 
-        object ISystemObjectManager.GetFieldContents(string fieldName)
-        {
-            throw new NotImplementedException();
-        }
-
-        string ISystemObjectManager.GetObjectName()
-        {
-            throw new NotImplementedException();
-        }
-
-        void ISystemObjectManager.SetObjectName(string objectName)
+        void I_SystemObjectManager.SetObjectName(string objectName)
         {
             throw new NotImplementedException();
         }
