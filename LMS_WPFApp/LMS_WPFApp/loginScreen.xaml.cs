4e0249dd7e2d3df0c6843c198ad2399949766046
th5528e <th5528e@gre.ac.uk>
2024-02-29 10:24:57 +0000

    updated cs files for functionality

--- LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
@@ -1,3 +1,17 @@
 ﻿using System.Windows;
 
+namespace LMS_WPFApp
+{
+    public partial class loginScreen : Window
+    {
+        private void loginButton_Click_1(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- LMS_WPFApp/LMS_WPFApp/studentMenu.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/studentMenu.xaml.cs
@@ -1,3 +1,22 @@
 ﻿using System.Windows;
 
+namespace LMS_WPFApp
+{
+    public partial class studentMenu : Window
+    {
+        private void payFeesButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void logoutStudentMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void inventoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
