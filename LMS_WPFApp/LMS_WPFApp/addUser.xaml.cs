5f6879ebef8f2718bea6e91b90ed1bcd60e96df1
th5528e <th5528e@gre.ac.uk>
2024-03-21 10:55:43 +0000

    User Database update, added addUser screen.

---
+++ LMS_WPFApp/LMS_WPFApp/addUser.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LMS_WPFApp
+{
+    public partial class AddUser : Window
+    {
+        private void createUserButton_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteUserButton_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
