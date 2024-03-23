92ced9ee628ad69102ca48940061a4b968329ec8
th5528e <th5528e@gre.ac.uk>
2024-03-23 20:03:11 +0000

    Added functionality to the teacherMenu for methods for adding people to the database. More functionality is needed here, adding tomorrow.

--- LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
@@ -1,17 +1,27 @@
 ﻿using System.Windows;
+using System.Windows.Navigation;
 
 namespace LMS_WPFApp
 {
     public partial class loginScreen : Window
     {
+        public loginScreen()
+        {
+            InitializeComponent();
+        }
+
         private void loginButton_Click_1(object sender, RoutedEventArgs e)
         {
-
+            teacherMenu teacherMenu = new teacherMenu();
+            teacherMenu.Show();
+            Close();
         }
 
         private void quitButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+
     }
 }
