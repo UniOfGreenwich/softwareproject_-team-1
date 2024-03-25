e9928fe0b65b25910d3850381dd8d48c69b76d49
Jake Willshire <jake.willshire@matthey.com>
2024-03-25 15:13:48 +0000

    Changed logic in loginScreen.xaml.cs to reflect new UserManager class.

--- LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/loginScreen.xaml.cs
@@ -5,14 +5,18 @@
 using System.IO;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace LMS_WPFApp
 {
     public partial class loginScreen : Window
     {
+        UserManager Users = new UserManager();
+
         public loginScreen()
         {
             InitializeComponent();
+            
         }
 =======
 using System.Windows.Navigation;
@@ -37,6 +41,7 @@ namespace LMS_WPFApp
         {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 <<<<<<< HEAD
         private void loginButton_Click(object sender, RoutedEventArgs e)
@@ -44,35 +49,65 @@ namespace LMS_WPFApp
 =======
 >>>>>>> 6544e31 (Added user creation, user deletion, login system, finished teacher menu basics)
             
+=======
+>>>>>>> 035a63d (Changed logic in loginScreen.xaml.cs to reflect new UserManager class.)
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Password;
 
+            // Is authenticate needed? We can split the following logic into its own "Authenticate method". Speak to George/Toby
             
-            bool isAuthenticated = AuthenticateUser(username, password);
-
-            if (isAuthenticated)
+            // Checks if user exists in database.
+            if (Users.FindObjectInList(username) == -1)
             {
-                
-                string accessLevel = GetUserAccessLevel(username);
-
-                
-                if (accessLevel == "1")
-                {
-                    teacherMenu teacherMenu = new teacherMenu();
-                    teacherMenu.Show();
-                }
-                else if (accessLevel == "0")
-                {
-                    studentMenu studentMenu = new studentMenu();
-                    studentMenu.Show();
-                }
-
-                
-                Close();
+                throw new Exception("Username not found. Are you a bot?");
             }
-            else
+
+            // Checks if password is null or password is not equal to value returned from database.
+            if (password == null || password != Users.GetSpecificObjectData(username, "Password"))
             {
+                throw new Exception("Invalid password! Try again..");
+            }
+
+            // Gets access level from Users. Switch statement to choose which menu based on access.
+            switch(Int32.Parse(Users.GetSpecificObjectData(username, "AccessLevel")))
+            {
+                default:
+                    studentMenu studentMenu = new studentMenu();
+                    break;
+
+                case 1:
+                    teacherMenu teacherMenu = new teacherMenu();
+                    break;
+            }
+
+            // Close window after login?
+            Close();
+            
+            //bool isAuthenticated = AuthenticateUser(username, password);
+
+            //if (isAuthenticated)
+            //{
                 
+            //    string accessLevel = GetUserAccessLevel(username);
+                
+            //    if (accessLevel == "1")
+            //    {
+            //        teacherMenu teacherMenu = new teacherMenu();
+            //        teacherMenu.Show();
+            //    }
+            //    else if (accessLevel == "0")
+            //    {
+            //        studentMenu studentMenu = new studentMenu();
+            //        studentMenu.Show();
+            //    }
+
+                
+            //    Close();
+            //}
+            //else
+            //{
+                
+<<<<<<< HEAD
                 MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 <<<<<<< HEAD
@@ -85,8 +120,13 @@ namespace LMS_WPFApp
 >>>>>>> 832661b (Added functionality to the teacherMenu for methods for adding people to the database. More functionality is needed here, adding tomorrow.)
 =======
 >>>>>>> 6544e31 (Added user creation, user deletion, login system, finished teacher menu basics)
+=======
+            //    MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+>>>>>>> 035a63d (Changed logic in loginScreen.xaml.cs to reflect new UserManager class.)
         }
 
+        // Closes application window if quit pressed
         private void quitButton_Click(object sender, RoutedEventArgs e)
         {
 <<<<<<< HEAD
@@ -94,32 +134,26 @@ namespace LMS_WPFApp
             Close();
         }
 
-        
+        //TODO @george to update when authentication and password hashing complete. Is this method needed?
         private bool AuthenticateUser(string username, string password)
         {
-        
             string filePath = "userDatabase.csv";
-
-        
             string[] lines = File.ReadAllLines(filePath);
             
-        
             foreach (string line in lines.Skip(1))
             {
                 string[] fields = line.Split(',');
-
                 string storedUsername = fields[0];
                 string storedPassword = fields[1];
 
-                
                 if (username == storedUsername && password == storedPassword)
                 {
                     return true;
                 }
             }
-
             return false;
         }
+<<<<<<< HEAD
 
 
         private string GetUserAccessLevel(string username)
@@ -150,6 +184,8 @@ namespace LMS_WPFApp
 
 <<<<<<< HEAD
 
+=======
+>>>>>>> 035a63d (Changed logic in loginScreen.xaml.cs to reflect new UserManager class.)
     }
 }
 =======
