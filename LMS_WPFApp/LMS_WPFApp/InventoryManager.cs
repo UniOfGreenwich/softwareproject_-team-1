08f2188ee8b0121359bfde6612be2254f77173b1
Jake Willshire <jake.willshire@matthey.com>
2024-03-25 12:16:23 +0000

    Inventory management logic completed

--- LMS_WPFApp/LMS_WPFApp/InventoryManager.cs
+++ LMS_WPFApp/LMS_WPFApp/InventoryManager.cs
@@ -22,12 +22,16 @@ namespace LMS_WPFApp
 
         void I_SystemObjectManager.OpenDatabaseFile()
         {
+            // open file using StreamReader class
             using (var reader = new StreamReader(databasePath))
             {
+                // create string list for storing file data
                 this.inventoryList = new List<List<string>>();
+                // create header list for column headers
                 this.tableHeaders = new List<string>();
                 while (!reader.EndOfStream)
                 {
+                    // splits values from file by commas
                     var line = reader.ReadLine();
                     var values = line.Split(',');
                     List<string> rowValues = new List<string>();
@@ -41,23 +45,19 @@ namespace LMS_WPFApp
                 reader.Close();
             }
             this.tableHeaders = inventoryList[0];
+            inventoryList.RemoveAt(0);
         }
         void I_SystemObjectManager.CloseDatabaseFile(List<List<string>> inventoryList)
         {
-            string inventoryString;
-            string rowString;
-
+            List<string> rowStringList = new List<string>();
             using (var writer = new StreamWriter(databasePath))
             {
                 foreach (var row in inventoryList)
                 {
-                    foreach (var value in row)
-                    {
-                        string valueString = value.ToString();
-                        rowString += (',' + valueString);
-                    }
-                    inventoryString += rowString + '\n';
+                    string rowCommaString = String.Join(",", row);
+                    rowStringList.Add(rowCommaString);
                 }
+                string inventoryString = String.Join("\n", rowStringList); 
                 writer.Write(inventoryString);
                 writer.Close();
             }
@@ -68,43 +68,84 @@ namespace LMS_WPFApp
             tableHeaders.Add(fieldName);
             foreach (var row in inventoryList)
             {
-                row.Add("");
+                row.Insert((row.Count() + 1), "");
             }
         }
 
-        void I_SystemObjectManager.CreateNewObject(string objectName)
+        void I_SystemObjectManager.CreateNewObject(List<string> objectItems)
         {
-            tableHeaders.ForEach(tableHeaders.Add();
+            // check if objectItems list is empty or values are null
+            if (objectItems == null || objectItems.Count == 0 || objectItems[0] == null)
+            {
+                throw new ArgumentNullException("CreateNewObject", "Object item list cannot be null");
+            }
+            string objectName = objectItems[0];
 
+            // Check if an object with the same name already exists
+            if (inventoryList.Any(item => item.Count > 0 && item[0] == objectName))
+            {
+                throw new ArgumentException("An object with the same name already exists in the inventory.");
+            }
+
+            // If no issues, add the new object to the inventory
+            inventoryList.Add(objectItems);
         }
 
         void I_SystemObjectManager.DeleteField(string fieldName)
         {
-            throw new NotImplementedException();
+            int deletionIndex = tableHeaders.IndexOf(fieldName);
+
+            if (deletionIndex != -1)
+            {
+                foreach (var row in inventoryList)
+                {
+                    row.RemoveAt(deletionIndex);
+                    //this.inventoryList.Insert(inventoryList.IndexOf(row), row);
+                }
+                tableHeaders.RemoveAt(deletionIndex);
+            }
+            else
+            {
+                //item does not exist for deletion
+            }
         }
 
         void I_SystemObjectManager.DeleteObject(string objectName)
         {
-            throw new NotImplementedException();
+            inventoryList.RemoveAt(FindObjectInList(objectName));
         }
 
-        object I_SystemObjectManager.GetFieldContents(string fieldName)
+        List<string> I_SystemObjectManager.GetObjectInfo(string objectName)
         {
-            inventoryList.Find(fieldName);
+            return inventoryList[FindObjectInList(objectName)];    
         }
 
         void I_SystemObjectManager.EditObject(string objectName, List<string> editedItems)
         {
-            try
+            if(FindObjectInList(objectName) == -1 || objectName != null)
             {
-                int foundIndex = inventoryList.FindIndex(objectName);
-                inventoryList.RemoveAt(foundIndex);
-                inventoryList.Insert(foundIndex, editedItems);
+                throw new ArgumentNullException("EditObject", "Object not found in table.");
             }
-            catch
+
+            if(editedItems == null)
             {
-                throw new Exception("Object not found!");
+                throw new ArgumentNullException("EditObject", "Edited items cannot be null");
             }
+
+            // do as normal if no errors
+            int foundIndex = FindObjectInList(objectName);
+            inventoryList.RemoveAt(foundIndex);
+            inventoryList.Insert(foundIndex, editedItems);
+
+        }
+        int FindObjectInList(string objectName)
+        {
+            List<string> objectNameColumn = new List<string>();
+            for (int i = 0; i < inventoryList.Count; i++)
+            {
+                objectNameColumn.Add(inventoryList[i][0]);
+            }
+            return objectNameColumn.FindIndex(name => name == objectName);
         }
     }
 }
