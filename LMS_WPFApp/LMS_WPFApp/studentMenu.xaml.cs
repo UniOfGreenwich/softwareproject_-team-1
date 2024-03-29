306453d81899e6f9634e403e2e99fce168e974ff
shuvo00000 <as5302e@gre.ac.uk>
2024-03-29 03:33:32 +0000

    updates of paymentGateway

--- LMS_WPFApp/LMS_WPFApp/studentMenu.xaml.cs
+++ LMS_WPFApp/LMS_WPFApp/studentMenu.xaml.cs
@@ -7,15 +7,21 @@ namespace LMS_WPFApp
         private UserManager Users;
         private InventoryManager Inventory;
         private List<string> userData;
+        private string username;
         public studentMenu(UserManager users, InventoryManager inventory, string username)
         {
             InitializeComponent();
             this.Users = users;
             this.Inventory = inventory;
             userData = Users.GetObjectInfo(username);
+            this.username = username;
         }
         private void payFeesButton_Click(object sender, RoutedEventArgs e)
         {
+            paymentGateway paymentWindow = new paymentGateway(Users, Inventory,username);
+            paymentWindow.Show();
+
+            Close();
             int newBalance = 0; //shuvo change this when you finish payment methods. need a return value for new balance
             Users.EditObject(userData[0], newBalance.ToString(), "balance");
             return;
