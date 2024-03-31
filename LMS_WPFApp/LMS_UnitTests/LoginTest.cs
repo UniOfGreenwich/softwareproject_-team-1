8a3e3179b43038e2a1a714a705d44f73c5c54e83
Repi909 <j.willshire@ntlworld.com>
2024-03-31 14:48:27 +0100

    Add: Unit tests for Login check and StudentMenu open/close

--- LMS_WPFApp/LMS_UnitTests/LoginTest.cs
+++ LMS_WPFApp/LMS_UnitTests/LoginTest.cs
@@ -6,20 +6,23 @@ namespace LMS_UnitTests
     [TestClass]
     public class LoginScreenTests
     {
+        private const string DatabasePath = "userDatabase.csv";
+
         [TestMethod]
         public void LoginUserPass()
         {
-            UserManager mockUserManager = new UserManager();
+            UserManager testUserManager = new UserManager();
+            testUserManager.OpenDatabaseFile();
             string username = "jd1234a";
-            string password = UserManager.ToSHA512("bed4efa1d4fdbd954bd3705d6a2a7827ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c79de74e17ecfec3cb6ac4bf");
+            string password = UserManager.ToSHA512("password123");
 
-            if (mockUserManager.FindObjectInList(username) == -1)
+            if (testUserManager.FindObjectInList(username) == -1)
             {
                 Assert.Fail();
             }
 
             // Checks if password is null or password is not equal to value returned from database.
-            if (password == null || password != mockUserManager.GetSpecificObjectData(username, "password"))
+            if (password == null || password != testUserManager.GetSpecificObjectData(username, "password"))
             {
                 Assert.Fail();
             }
