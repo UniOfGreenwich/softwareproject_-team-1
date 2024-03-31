32b89e102d5e254192a5225bd61091d7df76bc64
Repi909 <j.willshire@ntlworld.com>
2024-03-31 13:13:04 +0100

    Add: Test case for login. Checks username and password from database with login function

--- LMS_WPFApp/LMS_UnitTests/LoginTest.cs
+++ LMS_WPFApp/LMS_UnitTests/LoginTest.cs
@@ -1,11 +1,29 @@
+using LMS_WPFApp;
+using System.Windows;
+
 namespace LMS_UnitTests
 {
     [TestClass]
-    public class LoginTest
+    public class LoginScreenTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoginUserPass()
         {
+            UserManager mockUserManager = new UserManager();
+            string username = "jd1234a";
+            string password = UserManager.ToSHA512("bed4efa1d4fdbd954bd3705d6a2a7827ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c79de74e17ecfec3cb6ac4bf");
+
+            if (mockUserManager.FindObjectInList(username) == -1)
+            {
+                Assert.Fail();
+            }
+
+            // Checks if password is null or password is not equal to value returned from database.
+            if (password == null || password != mockUserManager.GetSpecificObjectData(username, "password"))
+            {
+                Assert.Fail();
+            }
+
         }
     }
 }
